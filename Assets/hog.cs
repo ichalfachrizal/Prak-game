1818a9ab4c8b07521a131d501bf687ccc1f6d63a
fachrizal <60200112036@uin-alauddin.ac.id>
2015-04-27 19:54:57 +0700

    laporan2

---
+++ Assets/hog.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent <Rigidbody> ().velocity = Vector3.zero;
+			GetComponent <Rigidbody> ().AddForce (Vector3.up * 350);
+		}
+		if (Input.GetKeyDown (KeyCode.RightControl)) {
+			GetComponent <Rigidbody> ().velocity = Vector3.zero;
+			GetComponent <Rigidbody> ().AddForce (Vector3.right * 300);
+		}
+		if (Input.GetKeyDown (KeyCode.LeftControl)) {
+			GetComponent <Rigidbody> ().velocity = Vector3.zero;
+			GetComponent <Rigidbody> ().AddForce (Vector3.left * 300);
+		}
+	}
+}
